daa672c4b4f93089e542ab6d6e689e569a988942
Zephyr Vi Britannia <zephyr1338@hotmail.com>
2016-04-21 18:55:14 +0100

    Create example.js
    An example file to show prospective programmers how this client will work

---
+++ example.js
@@ -0,0 +1,27 @@
+
+var server = require('./slither/servers');
+var client = require('./slither/slitherio-client');
+
+server.randomServer(function(server)
+{
+    console.log('retreived server:', server);
+    
+    var slitherClient = new client('slitherino.js', server);
+
+    slitherClient.on('snakeJoined', function()
+    {
+        console.log(slitherClient.nickname, 'has joined the game.');
+    });
+
+    slitherClient.on('newFood', function(dataBuffer)
+    {
+        //to be added
+    });
+
+    slitherClient.on('snakeAppeared', function(currentSnake)
+    {
+        console.log(currentSnake);
+    });
+
+    slitherClient.start();
+});
