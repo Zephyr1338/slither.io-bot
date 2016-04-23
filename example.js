dd9856058dba53e07ae5756c6f5e8559a037e247
Zephyr Vi Britannia <zephyr1338@hotmail.com>
2016-04-23 13:26:14 +0100

    Update example.js to reflect README.md's changes

--- example.js
+++ example.js
@@ -1,25 +1,37 @@
+
 var server = require('./slither/servers');
 var client = require('./slither/slitherio-client');
 
 server.randomServer(function(server)
 {
     console.log('retreived server:', server);
-    
+
     var slitherClient = new client('slitherino.js', server);
 
     slitherClient.on('snakeJoined', function()
     {
         console.log(slitherClient.nickname, 'has joined the game.');
+        slitherClient.move(100, 100);
     });
 
-    slitherClient.on('newFood', function(dataBuffer)
+    slitherClient.on('foodSpawned', function(spawnedFood)
     {
-        //to be added
+        console.log('spawned', spawnedFood);
     });
 
-    slitherClient.on('snakeAppeared', function(currentSnake)
+    slitherClient.on('foodEaten', function(eaterSnake)
     {
-        console.log(currentSnake);
+        console.log('eaten', eaterSnake);
+    });
+
+    slitherClient.on('snakeAppeared', function(appearedSnake)
+    {
+        //console.log('appeared', appearedSnake);
+    });
+
+    slitherClient.on('snakeMoved', function(movedSnake, movedX, movedY)
+    {
+        console.log(movedSnake, movedX, movedY);
     });
 
     slitherClient.start();
