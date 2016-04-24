8c7cb72dc464a13412610efc87e58662ab8ed78b
Zephyr Vi Britannia <zephyr1338@hotmail.com>
2016-04-24 19:25:13 +0100

    Update example.js

--- example.js
+++ example.js
@@ -1,4 +1,4 @@
-
+var util = require('util');
 var server = require('./slither/servers');
 var client = require('./slither/slitherio-client');
 
@@ -11,27 +11,27 @@ server.randomServer(function(server)
     slitherClient.on('snakeJoined', function()
     {
         console.log(slitherClient.nickname, 'has joined the game.');
-        slitherClient.move(100, 100);
+        slitherClient.move(0, 0);
     });
 
     slitherClient.on('foodSpawned', function(spawnedFood)
     {
-        console.log('spawned', spawnedFood);
+        console.log(util.format('[%s] has spawned with mass of [%s] at (%s, %s).', spawnedFood.id, spawnedFood.sz, spawnedFood.xx, spawnedFood.yy));
     });
 
-    slitherClient.on('foodEaten', function(eaterSnake)
+    slitherClient.on('foodEaten', function(eaterSnake, eatenFood)
     {
-        console.log('eaten', eaterSnake);
+        console.log(util.format('[%s] has eaten mass of [%s].', eaterSnake.nickname, eaterFood.sz));
     });
 
     slitherClient.on('snakeAppeared', function(appearedSnake)
     {
-        //console.log('appeared', appearedSnake);
+        console.log(util.format('[%s] has currently spawned at (%s, %s).', appearedSnake.nickname, appearedSnake.xx, appearedSnake.yy));
     });
 
     slitherClient.on('snakeMoved', function(movedSnake, movedX, movedY)
     {
-        console.log(movedSnake, movedX, movedY);
+        console.log(util.format('[%s] has currently moved to (%s, %s).', movedSnake.nickname, movedX, movedY));
     });
 
     slitherClient.start();
