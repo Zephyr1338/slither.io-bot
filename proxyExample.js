ddb69275bc4b127f7a2c239b4a1230344cd10e54
Zephyr Vi Britannia <zephyr1338@hotmail.com>
2016-04-25 16:02:05 +0100

    Update proxyExample.js

--- proxyExample.js
+++ proxyExample.js
@@ -10,35 +10,34 @@ fs.readFile('proxies.txt', 'utf8', function(error, data)
 
 	for(var i = 0; i < proxies.length; i++)
 	{
-		var slitherClient = new client('slither.js', server);
+	    var slitherClient = new client('slitherino.js', server);
+	
+	    slitherClient.on('snakeJoined', function()
+	    {
+	        console.log(slitherClient.nickname, 'has joined the game.');
+	        slitherClient.move(0, 0);
+	    });
+	
+	    slitherClient.on('foodSpawned', function(spawnedFood)
+	    {
+	        console.log(util.format('[%s] has spawned with mass of [%s] at (%s, %s).', spawnedFood.id, spawnedFood.sz, spawnedFood.xx, spawnedFood.yy));
+	    });
+	
+	    slitherClient.on('foodEaten', function(eaterSnake, eatenFood)
+	    {
+	        console.log(util.format('[%s] has eaten mass of [%s].', eaterSnake.nickname, eaterFood.sz));
+	    });
+	
+	    slitherClient.on('snakeAppeared', function(appearedSnake)
+	    {
+	        console.log(util.format('[%s] has currently spawned at (%s, %s).', appearedSnake.nickname, appearedSnake.xx, appearedSnake.yy));
+	    });
+	
+	    slitherClient.on('snakeMoved', function(movedSnake, movedX, movedY)
+	    {
+	        console.log(util.format('[%s] has currently moved to (%s, %s).', movedSnake.nickname, movedX, movedY));
+	    });
 
-		slitherClient.on('snakeJoined', function()
-		{
-			console.log(slitherClient.nickname, 'has joined the game.');
-			//slitherClient.move(100, 100);
-			//slitherClient.setAccelerate(true);
-		});
-
-		slitherClient.on('foodSpawned', function(spawnedFood)
-		{
-			//console.log('spawned', spawnedFood);
-		});
-
-		slitherClient.on('foodEaten', function(eaterSnake)
-		{
-			//console.log('eaten', eaterSnake);
-		});
-
-		slitherClient.on('snakeAppeared', function(appearedSnake)
-		{
-			//console.log('appeared', appearedSnake);
-		});
-
-		slitherClient.on('snakeMoved', function(movedSnake, movedX, movedY)
-		{
-			//console.log(movedSnake, movedX, movedY);
-		});
-
-		slitherClient.start(proxies[i]);
+	    slitherClient.start(proxies[i]);
 	}
 });
