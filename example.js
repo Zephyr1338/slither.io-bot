7bf6561026216ebdd09a2e6cc884f0173f946f21
Zephyr Vi Britannia <zephyr1338@hotmail.com>
2016-05-01 20:59:54 +0100

    Updated to include new event handler "codeUpdate"

--- example.js
+++ example.js
@@ -4,34 +4,40 @@ var client = require('./slither/slitherio-client');
 
 server.randomServer(function(server)
 {
-    console.log('retreived server:', server);
+    console.log('retrieved server:', server);
 
     var slitherClient = new client('slitherino.js', server);
 
     slitherClient.on('snakeJoined', function()
     {
-        console.log(slitherClient.nickname, 'has joined the game.');
-        slitherClient.move(0, 0);
+        console.log(util.format('[%s] has joined the game.', slitherClient.nickname));
     });
 
     slitherClient.on('foodSpawned', function(spawnedFood)
     {
         console.log(util.format('[%s] has spawned with mass of [%s] at (%s, %s).', spawnedFood.id, spawnedFood.sz, spawnedFood.xx, spawnedFood.yy));
+        slitherClient.move(spawnedFood.xx, spawnedFood.yy);
     });
 
     slitherClient.on('foodEaten', function(eaterSnake, eatenFood)
     {
         console.log(util.format('[%s] has eaten mass of [%s].', eaterSnake.nickname, eaterFood.sz));
-    });
+	});
 
     slitherClient.on('snakeAppeared', function(appearedSnake)
     {
-        console.log(util.format('[%s] has currently spawned at (%s, %s).', appearedSnake.nickname, appearedSnake.xx, appearedSnake.yy));
+        console.log(util.format('[%s] has spawned at (%s, %s).', appearedSnake.nickname, appearedSnake.xx, appearedSnake.yy));
+
     });
 
-    slitherClient.on('snakeMoved', function(movedSnake, movedX, movedY)
+    slitherClient.on('snakeMoved', function(movedSnake)
     {
-        console.log(util.format('[%s] has currently moved to (%s, %s).', movedSnake.nickname, movedX, movedY));
+        console.log(util.format('[%s] has moved to (%s, %s).', movedSnake.nickname, movedSnake.xx, movedSnake.yy));
+    });
+
+    slitherClient.on('codeUpdate', function(protocolVersion)
+    {
+        console.log('The code protocol has been update to ' + protocolVersion);
     });
 
     slitherClient.start();
